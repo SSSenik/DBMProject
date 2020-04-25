3a8def73ef8251efe429985c5493badcc8582b84
Diogo Sénico <senico.benfica@gmail.com>
2020-04-25 22:31:17 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(8081, () => {
+  const host =
+    server.address().address === '::' ? 'localhost' : server.address().address;
+  const { port } = server.address();
+  console.log('Example app listening at http://%s:%s', host, port);
+});
